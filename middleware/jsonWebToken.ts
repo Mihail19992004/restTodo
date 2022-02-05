a3fdf1591902cb3a478e8bfaac5cbb3d709d3190
misha <i.misha.kam@gmail.com>
2022-02-05 23:29:33 +0300

    'add_error_handler'

--- middleware/jsonWebToken.ts
+++ middleware/jsonWebToken.ts
@@ -2,19 +2,22 @@ import {sign, verify} from 'jsonwebtoken'
 import {Request, Response, NextFunction} from 'express'
 
 export function authenticateToken(req: Request, res: Response, next: NextFunction) {
+    try {
+        const authHeader = req.headers['authorization']
+        const token = authHeader && authHeader.split(' ')[1]
 
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
+        if (token == null) return res.status(401).json({message: 'Authorized request'})
 
-    if (token == null) return res.status(401).json({message: 'Authorized request'})
+        verify(token, 'secret', (err: any, user: any) => {
+            if (err) return res.status(403).json({message: 'Authorized request'})
+            // @ts-ignore
+            req.user = user.id
+            next()
+        })
+    } catch (error) {
+        res.status(500).json({message: 'Authorization error. Please try again later'})
+    }
 
-    verify(token, 'secret', (err: any, user: any) => {
-        if (err) return res.status(403).json({message: 'Authorized request'})
-        // @ts-ignore
-        req.user = user
-
-        next()
-    })
 }
 
 export const generateAccessToken = (id: string) => {
